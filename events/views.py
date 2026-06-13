4ce0951ac8b70edfbf03fa11f9d5faf882ffdf1c
amal <amnerkailo@gmail.com>
2026-06-13 22:23:37 +0530

    Implement event listing api

--- events/views.py
+++ events/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Event
+from .serializers import EventSerializer
 
 # Create your views here.
+
+
+class EventListView(generics.ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
