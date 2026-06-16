f2da7cc76c3207eb0b15379738abe6283594def8
amal <amnerkailo@gmail.com>
2026-06-16 20:27:55 +0530

    Implement React authentication and event listing

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function Events() {
-  return <h2>Events Page</h2>;
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  const fetchEvents = async () => {
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:8000/api/events/"
+      );
+
+      setEvents(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Events</h2>
+
+      {events.map((event) => (
+        <div key={event.id}>
+          <h3>{event.title}</h3>
+          <p>{event.description}</p>
+          <p>{event.location}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Events;
