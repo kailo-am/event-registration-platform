6d5bfcf269683259c74a094969bb79d0cb3df613
amal <amnerkailo@gmail.com>
2026-06-16 20:25:11 +0530

    Implement React authentication pages

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,67 @@
+import { useState } from "react";
+import axios from "axios";
+
 function Register() {
-  return <h2>Register Page</h2>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post(
+        "http://127.0.0.1:8000/api/register/",
+        {
+          username,
+          email,
+          password,
+        }
+      );
+
+      alert("Registration Successful!");
+    } catch (error) {
+      alert("Registration Failed!");
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br /><br />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
