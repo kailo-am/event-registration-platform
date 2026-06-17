018fd7db48163b94c9eac2564ee2f1edf5ccb954
amal <amnerkailo@gmail.com>
2026-06-17 14:26:49 +0530

    Add home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Events />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/events" element={<Events />} />
